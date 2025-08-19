4a4682ad81ab0e8e4d5f5a3edbdc2f0470607a56
ksg <kasaga234@gmail.com>
2025-08-19 10:07:20 +0900

    016

--- DoIt/src/_016_내림차순으로자릿수정렬하기.java
+++ DoIt/src/_016_내림차순으로자릿수정렬하기.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class _016_내림차순으로자릿수정렬하기 {
     public static void main(String[] args) {
-        // 버블정렬 사용하기
+        // 선택정렬 사용하기
         // 연속된 숫자로 받기 12345 이런식으로 
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
@@ -10,13 +10,18 @@ public class _016_내림차순으로자릿수정렬하기 {
         for (int i = 0; i < str.length(); i++) {
             array[i] = Integer.parseInt(str.substring(i, i+1)); // 문자열을 숫자로 바꾸기
         }
+
         for (int i = 0; i < array.length-1 ; i++) {
-            for (int j = 0; j < array.length-1 - i; j++) {
-                if(array[j] < array[j+1]){
-                    int tmp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = tmp;
+            int max = i;
+            for (int j= i+1; j < array.length; j++) {
+                if(array[j] > array[max]){
+                    max = j;
                 }
+            }
+            if(array[i]< array[max]){
+                int tmp = array[max];
+                array[max] = array[i];
+                array[i]= tmp;
             }
         }
         for (int i = 0; i < array.length; i++) {
